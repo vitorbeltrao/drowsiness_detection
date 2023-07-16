49a7d0b927e45f11fab5cbe75323837aeec3b348
vitorbeltrao <vitorbeltraoo@hotmail.com>
2023-07-16 11:58:33 -0300

    🎨 Update test data component

--- components/03_test_data/test_data.py
+++ components/03_test_data/test_data.py
@@ -9,10 +9,11 @@ Date: July/2023
 # import necessary packages
 import logging
 import sys
-import cv2
-import matplotlib.pyplot as plt
+import wandb
+# import cv2
+# import matplotlib.pyplot as plt
 from ultralytics import YOLO
-from PIL import Image
+# from PIL import Image
 
 logging.basicConfig(
     level=logging.INFO,
@@ -24,37 +25,45 @@ YOLO_MODEL_PATH = sys.argv[1]
 IMAGE_PATH = sys.argv[2]
 
 
-def inference_image(image_path: str, yolo_model_path: str) -> None:
-    '''
-    Perform inference on an image.
+# def inference_image(image_path: str, yolo_model_path: str) -> None:
+#     '''
+#     Perform inference on an image.
 
-    Args:
-        image_path (str): The path to the input image.
-        yolo_model_path (str): The path to the YOLO model weights.
+#     Args:
+#         image_path (str): The path to the input image.
+#         yolo_model_path (str): The path to the YOLO model weights.
 
-    Returns:
-        None
-    '''
-    # Load the trained model
-    final_model = YOLO(yolo_model_path)
-    logging.info("Loading image: %s", image_path)
+#     Returns:
+#         None
+#     '''
+#     # Load the trained model
+#     final_model = YOLO(yolo_model_path)
+#     logging.info("Loading image: %s", image_path)
 
-    # Load the image
-    img = Image.open(image_path)
-    logging.info("Performing inference on the image...")
+#     # Load the image
+#     img = Image.open(image_path)
+#     logging.info("Performing inference on the image...")
 
-    # Perform inference
-    results = final_model(img)
-    logging.info("Inference completed. Displaying results.")
+#     # Perform inference
+#     results = final_model(img)
+#     logging.info("Inference completed. Displaying results.")
 
-    # Plot and display the results
-    res_plotted = results[0].plot()
-    recolor = cv2.cvtColor(res_plotted, cv2.COLOR_BGR2RGB)
-    plt.imshow(recolor)
-    plt.show()
+#     # Plot and display the results
+#     res_plotted = results[0].plot()
+#     recolor = cv2.cvtColor(res_plotted, cv2.COLOR_BGR2RGB)
+#     plt.imshow(recolor)
+#     plt.show()
 
 
 if __name__ == "__main__":
     logging.info('About to start executing the test data component\n')
-    inference_image(YOLO_MODEL_PATH, IMAGE_PATH)
+    # inference_image(YOLO_MODEL_PATH, IMAGE_PATH)
+    # save the results in wandb
+    run = wandb.init(
+        project='drowsiness_detection',
+        entity='vitorabdo',
+        job_type='Yolo test model')
+    run.finish()
+    
+    logging.info('Creating run for drowsiness detection: SUCCESS\n')
     logging.info('Done executing the test data component')
