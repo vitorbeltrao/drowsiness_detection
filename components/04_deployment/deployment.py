cd00a2828524056ef1a1de701abe1408a6e70af1
vitorbeltrao <vitorbeltraoo@hotmail.com>
2023-07-18 08:12:19 -0300

    🎨 Update deployment component

--- components/04_deployment/deployment.py
+++ components/04_deployment/deployment.py
@@ -24,9 +24,7 @@ FINAL_MODEL = sys.argv[2]
 
 
 def deploy_model(prod_deployment_path: str, 
-                 final_model: str,
-                 latest_scores_path: str,
-                 ingested_files_path: str) -> None:
+                 final_model: str) -> None:
     '''Function for deployment, copy the latest pickle file, the latestscore.txt value, 
     and the ingestedfiles.txt file into the deployment directory
 
@@ -34,32 +32,26 @@ def deploy_model(prod_deployment_path: str,
     Folder in the main directory where the production files will be copied
 
     :param final_model: (pickle)
-    Pickle file with all saved model pipeline
-
-    :param latest_scores_path: (str)
-    Directory of the "06_test_model" folder where the latest model score file is located
-
-    :param ingested_files_path: (str)
-    Directory of the "01_upload_raw_data" folder where the "ingested_files" is located
+    Yolo weights file with all saved model pipeline
     '''
     logging.info(
-        f'Model deployment. Model, its latest score, and the list of files used for training are saved in '
+        f'Model deployment. Lets save the best trained model in '
         f'{prod_deployment_path}')
 
     # obtaining the necessary files
-    # 1. final model pickle
     run = wandb.init(
         project='risk_assessment',
         entity='vitorabdo',
         job_type='deployment')
     model_local_path = run.use_artifact(final_model, type='pt').download()
+    run.finish()
  
     # create production deployment folder if it doesnt exists
     if not os.path.isdir(prod_deployment_path):
         os.mkdir(prod_deployment_path)
 
     # copy files to the production deployment folder
-    shutil.copy(os.path.join(model_local_path, 'best_model.pt'), prod_deployment_path) # copy pickle
+    shutil.copy(os.path.join(model_local_path, 'best_model_pipe.pt'), prod_deployment_path) # copy .pt file
     logging.info('Copied files: SUCCESS')
 
 
